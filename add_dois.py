f6c5f8569bc434314e86b5b0be7bee20ff72adc2
Niko McCarty <nsmccarty3@gmail.com>
2021-10-21 17:09:19 -0400

    worked on add_doi, attempts at accessing data

--- add_dois.py
+++ add_dois.py
@@ -1,58 +1,3 @@
-
-
-# articleList = []
-
-# Entrez.email = 'nsmccarty3@gmail.com'
-
-# handle = Entrez.esearch(db='pubmed',
-#                         retmax='5000',
-#                         term='("autism") AND (("2020/09/10"[Date - Publication] : "3000"[Date - Publication])')
-# record = Entrez.read(handle)
-# handle.close()
-# idlist = record['IdList']
-
-# print("ID List completed. Moving to Medline...")
-
-# # Now that we have a list of PubMed IDs for all required articles, we will use Medline to extract information from them. 
-# handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline",
-#                        retmode="text")
-# records = Medline.parse(handle)
-
-# recordList = []
-# for record in records:
-#     affiliations = []
-#     for i in record.get("AD", "NaN"):
-#         if i not in affiliations:
-#             affiliations.append(i)
-#         else:
-#             continue
-
-#     for j in record.get("AID", "NaN"):
-#         try:
-#             start = j.find("10.") + len("10.")
-#             end = j.find(" [doi]")
-#             doi_string = "10." + j[start:end]
-#             # Remove the added [pii in some strings, from the AID call. 
-#             new_doi_string = doi_string.replace(" [pii", "")
-#         except AttributeError:
-#             new_doi_string = "NaN"
-
-#     recordList.append({
-#         "title:": str(record.get("TI", "NaN")),
-#         "authors": str(record.get("FAU", "NaN")),
-#         "doi": "https://doi.org/" + new_doi_string,
-#         "affiliations": str(affiliations),
-#         "journal_abbrev": str(record.get("TA", "NaN")),
-#         "pub_date": str(record.get("DP", "NaN"))
-#      })
-
-# print("Medline information extracted. Exporting .csv file with data...")
-
-# # # Generate Pandas DataFrame from list of dictionaries
-# articlesPD = pd.DataFrame.from_dict(recordList)
-# export_csv = articlesPD.to_csv('pubmed_papers_doi.csv', index = None, header=True)
-# print("Task complete. Closing.")
-
 from pymed import PubMed
 import numpy as np
 import pandas as pd
@@ -60,6 +5,7 @@ from functions import *
 from bs4 import BeautifulSoup
 import requests
 import time
+import json
 
 print("Executing script...")
 df = pd.read_csv('datasets/20211015_drug_trials_master.csv')
@@ -69,34 +15,73 @@ NCTid_list = df['NCTId'].tolist()
 print("The list of NCTids includes " + str(len(NCTid_list)) + " records.")
 
 articleDois = []
+articleNCTids = []
 
 def retrieve_DOIs(NCTid):
-    result = pubmed.query(NCTid, max_results=20)
+    results = pubmed.query(NCTid, max_results=20)
 
-    articleList = []
-    articleDoi_temp = []
+    if pubmed.getTotalResultsCount(NCTid) == 0:
+        articleDois.append('None found.')
+        articleNCTids.append(NCTid)
+    elif pubmed.getTotalResultsCount(NCTid) > 0:
+        endstring = ''
+        for article in results:
+            endstring += article
+            endstring += '|'
+        articleDois.append(endstring)
+    else:
+        articleDois.append("Error.")
 
-    for article in result:
-        articleDict = article.toDict()
-        articleList.append(articleDict)
+    articleNCTids.append(NCTid)
 
-    for article in articleList:
-        if len(articleList) == 0:
-            articleDois.append({u'doi':'None Found'})
-        elif len(articleList) == 1:
-            articleDois.append({u'doi':article['doi']})
-        elif len(articleList) > 1:
-            articleDoi_temp.append(article['doi'] + '|')
-            articleDois.append({u'doi':articleDoi_temp})
-        else:
-            continue
-    time.sleep(1)
-    print("Loop complete.")
+    print(articleDois)
+    print(articleNCTids)
 
-for item in NCTid_list:
-    retrieve_DOIs(item)
+    # articleList = []
+    # articleDoi_temp = []
+
+    # if result:
+    #     print(result)
+        # for article in result:
+        #     articleDict = article.toDict()
+        #     articleList.append(articleDict) #now articleList contains the articles, in JSON format, possibly with multiple entries
+
+        #     if len(articleList) == 0:
+        #         articleDois.append('None Found')
+        #         print("The length is zero")
+        #     elif len(articleList) == 1:
+        #         articleDois.append({article['doi']})
+        #         print("The length is one.")
+        #     elif len(articleList) > 1:
+        #         articleDoi_temp.append(article['doi'])
+        #         del articleDoi_temp[1:]
+        #         articleDois.append({article[NCTid]: articleDoi_temp})
+        #         print("The length is greater than one.")
+        #         pass
+        #     else:
+        #         continue
+
+    # else:  
+    #     articleDois.append('None Found')
+    #     print("The length is zero")
+
+    # author_metrics.append({
+    #         "headline": str(article_headline),
+    #         "hyperlink": str(link),
+    #         "article_date": str(article_date),
+    #         "word_count": str(total_words),
+    #         "number_authors": str(number_authors),
+    #         "author_adjusted_words": str(adjusted_words)
+    #     })
+
+    # print(list(articleList))
+    # print(articleDois)
+    # time.sleep(1)
+    # print("Loop complete.")
+
+retrieve_DOIs('NCT00844753')
 
 # Generate Pandas DataFrame from list of dictionaries
-articlesPD = pd.DataFrame.from_dict(articleDois)
-articlesPD['NCTids'] = pd.Series([NCTid_list])
-export_csv = articlesPD.to_csv('list_of_pubmed_papers.csv', index = None, header=True) 
+# articlesPD = pd.DataFrame.from_dict(articleDois)
+# articlesPD['NCTids'] = pd.Series([NCTid_list])
+# export_csv = articlesPD.to_csv('list_of_pubmed_papers.csv', index = None, header=True) 
